5bdb62e84557af02db50cf1cf06bbe8977cd64f3
earlscabanig <earlc.cabanig@gmail.com>
2024-02-23 20:51:34 +0800

    fixed saving on insertleave

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,8 +2,17 @@ vim.opt.clipboard:append("unnamedplus")
 vim.g.mapleader = " "
 
 local function save_on_insert_leave()
-	vim.cmd([[autocmd InsertLeave * silent! write]])
+    vim.api.nvim_create_autocmd("InsertLeave", {
+        pattern = "*",
+        callback = function()
+            -- Call Prettier format, and ensure the operation is silent
+            pcall(function() require("prettier").format() end)
+            -- Save the file silently
+            vim.cmd('silent! write')
+        end
+    })
 end
+
 save_on_insert_leave()
 
 vim.api.nvim_set_keymap('n', '<C-s>', '<cmd>lua require("prettier").format()<CR>:w<CR>', { noremap = true, silent = true })
