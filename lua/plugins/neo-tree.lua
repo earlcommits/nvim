ddf56e9d058104b727f34fbf51d51c099255f469
earlscabanig <earlc.cabanig@gmail.com>
2024-02-25 10:32:15 +0800

    auto close on file select

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -7,6 +7,19 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
+		require("neo-tree").setup({
+			event_handlers = {
+				{
+					event = "file_opened",
+					handler = function(file_path)
+						-- auto close
+						-- vimc.cmd("Neotree close")
+						-- OR
+						require("neo-tree.command").execute({ action = "close" })
+					end
+				},
+			}
+		})
 		vim.keymap.set('n', '<C-b>', ':Neotree filesystem reveal right<CR>', {})
 	end 
 }
