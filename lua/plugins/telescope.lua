22763df52646d7e2fc5a23f788ec4a4e69dd27cd
earlscabanig <earlc.cabanig@gmail.com>
2024-03-01 15:06:23 +0800

    find files correctly

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,8 @@
-
 return {'nvim-telescope/telescope.nvim', tag = '0.1.5',
 	dependencies = { 'nvim-lua/plenary.nvim' },
 	config = function()
-		local builtin = require("telescope.builtin")
-		vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+		local builtin = require("telescope.builtin") 
+		vim.keymap.set('n', '<C-p>', builtin.find_files({hidden =true}), {})
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 	end
 }
